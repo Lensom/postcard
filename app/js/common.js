5e00219f5a46ecc1c0eaa0b0984222b41d7d4f2b
unknown <magurawork@gmail.com>
2019-01-22 17:40:03 +0300

    first page without adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -10,4 +10,15 @@ $(function() {
 		return false;
 	});
 
+	// Клик на открытку
+
+	$('.card-v').on('click', function(e){	
+		if (!$(this).closest('.slot').hasClass('active')) {
+			$('.slot').removeClass('active');	
+			$(this).closest('.slot').addClass('active');
+		} else {
+			$('.slot').removeClass('active');
+		}				
+	})
+
 });
