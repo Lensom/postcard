6e2e7f7225ea36ec5dfa74fc004cb713e3687170
unknown <magurawork@gmail.com>
2019-01-29 17:40:10 +0300

    remove not correct og:url

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ $random_letters = $letter->getRandomLetters();
     <meta name="description" content="dostavka.by онлайн-гипермаркет">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta property="og:image" content="path/to/image.jpg">
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
     <!-- Chrome, Firefox OS and Opera -->
     <meta name="theme-color" content="#000">
